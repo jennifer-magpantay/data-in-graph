1008cdf3b6d6dca77e3417d164886c1dce2112a8
Jennifer Magpantay <jennifer.magpantay@gmail.com>
2022-06-07 14:11:53 +0200

    refactor: Refactor route list
    refactor: Refactor route list by adding screen elements to each item

--- project/src/routes/routes.tsx
+++ project/src/routes/routes.tsx
@@ -1,6 +1,27 @@
+// route screens
+import { Overview } from "../screens/Overview";
+import { StatsByAge } from "../screens/StatsByAge";
+import { StatsByGender } from "../screens/StatsByGender";
+import { StatsByIndustry } from "../screens/StatsByIndustry";
+
 export const routes = [
-  { id: 1, title: "Overview", path: "/" },
-  { id: 2, title: "Stats by age", path: "/stats-by-age" },
-  { id: 3, title: "Stats by gender", path: "/stats-by-gender" },
-  { id: 4, title: "Stats by industry", path: "/stats-by-industry" },
+  { id: 1, title: "Overview", path: "/", element: <Overview /> },
+  {
+    id: 2,
+    title: "Stats by age",
+    path: "/stats-by-age",
+    element: <StatsByAge />,
+  },
+  {
+    id: 3,
+    title: "Stats by gender",
+    path: "/stats-by-gender",
+    element: <StatsByGender />,
+  },
+  {
+    id: 4,
+    title: "Stats by industry",
+    path: "/stats-by-industry",
+    element: <StatsByIndustry />,
+  },
 ];
