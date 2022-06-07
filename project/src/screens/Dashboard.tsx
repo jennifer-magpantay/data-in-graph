b65cd17cf8bf8c80ee04d0d19b1997b690cbfbc1
Jennifer Magpantay <jennifer.magpantay@gmail.com>
2022-06-07 14:17:00 +0200

    refactor: Refactor Dashboard component to receive route content from sidebar

--- project/src/screens/Dashboard.tsx
+++ project/src/screens/Dashboard.tsx
@@ -1,26 +1,30 @@
-import React, { useState, useEffect, SyntheticEvent } from "react";
-import { Aside } from "../components/Aside";
+import { useState, useEffect, SyntheticEvent } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import { Sidebar } from "../components/Sidebar";
 import { Main } from "../components/Main";
 
 import { ColumnContainer } from "../components/ColumnContainer";
 import { Card } from "../components/Card";
+import { GraphContainer } from "../components/GraphContainer";
 import { LineGraph } from "../components/LineGraph";
 
 // data
-import { childrenAndTeenagersFoundData } from "../data/childrenAndTeenagersFound";
+import { minorsFoundData } from "../data/minorsFound";
 import { inspectionsData } from "../data/inspections";
+
+// data
+import { routes } from "../routes/routes";
+
 // types
 import {
   InspectionsDataType,
   ChildrenAndTeenagersFoundDataType,
-  DynamicDataType,
 } from "../types/types";
 
 // helpers
 import { addId } from "../helpers/addId";
 import { reduceData } from "../helpers/reduceData";
-import { GraphContainer } from "../components/GraphContainer";
-import { formatNumber } from "../helpers/formatNumber";
 
 export const Dashboard = () => {
   const [inspections, setInspections] = useState<InspectionsDataType[]>([]);
@@ -33,7 +37,7 @@ export const Dashboard = () => {
 
   // once page is loaded, set the states
   useEffect(() => {
-    const childAndTeenagersData = addId(childrenAndTeenagersFoundData);
+    const childAndTeenagersData = addId(minorsFoundData);
     setMinorsFound(childAndTeenagersData);
 
     const inspectionsDt = addId(inspectionsData);
@@ -54,68 +58,14 @@ export const Dashboard = () => {
 
   return (
     <div className="dashboard">
-      <Aside />
+      <Sidebar />
       <Main>
-        <ColumnContainer width="w-3/4" style="pr-5">
-          <GraphContainer
-            legend={
-              isDataInspectionDisplayed
-                ? "* Inspection actions in which the Labor Tax Audit found the work of children and adolescents below the minimum age allowed or the work of adolescents between 16 and 17 years old in conditions prohibited by legislation."
-                : "* Children and adolescents found by the Labor Tax Audit in a situation of child labor, either below the minimum age allowed for work or, in the case of adolescents between 16 and 17 years of age, in jobs prohibited by law."
-            }
-          >
-            <h3 className="graph--title">
-              {isDataInspectionDisplayed
-                ? "Inspections completed"
-                : "Minors found during inspections"}
-            </h3>
-            <LineGraph
-              // text=""
-              displayLabel={false}
-              labels={
-                isDataInspectionDisplayed
-                  ? inspections.map((item) => item.ano)
-                  : minorsFound.map((item) => item.ano)
-              }
-              // label=""
-              dataset={
-                isDataInspectionDisplayed
-                  ? inspections.map((item) => item.quantidade)
-                  : minorsFound.map((item) => item.quantidade)
-              }
-              color={isDataInspectionDisplayed ? "#1e3799" : "#079992"}
-            />
-          </GraphContainer>
-        </ColumnContainer>
-
-        <ColumnContainer width="w-1/4" style="h-full flex flex-col">
-          <button className="w-full" onClick={(e) => handleButtonClick(e)}>
-            Inspections completed
-          </button>
-          <button className="w-full" onClick={(e) => handleButtonClick(e)}>
-            Minors found
-          </button>
-
-          <Card
-            title={
-              isDataInspectionDisplayed
-                ? "Total of inspections completed in the period *"
-                : "Total of minors found during inspections *"
-            }
-            content={
-              isDataInspectionDisplayed
-                ? reduceData(inspections, "quantidade")
-                : reduceData(minorsFound, "quantidade")
-            }
-          />
-          <p className="caption">
-            * The data presented in the graphs are from 01/2017 to 04/2022
-          </p>
-          {/* <Card
-            title="Total of children and teenagers found"
-            content={reduceData(childrenAndTeenagersFound, "quantidade")}
-          /> */}
-        </ColumnContainer>
+        {/* routes */}
+        <Routes>
+          {routes.map((item) => (
+            <Route path={item.path} element={item.element} />
+          ))}
+        </Routes>
       </Main>
     </div>
   );
