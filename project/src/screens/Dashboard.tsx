662d7dd81fbd0f53a3909a9b87736d8c09a9767d
Jennifer Magpantay <jennifer.magpantay@gmail.com>
2022-06-08 17:56:32 +0200

    refactor: Refactor Dashboard screen

--- project/src/screens/Dashboard.tsx
+++ project/src/screens/Dashboard.tsx
@@ -1,61 +1,12 @@
-import { useState, useEffect, SyntheticEvent } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { Sidebar } from "../components/Sidebar";
 import { Main } from "../components/Main";
 
-import { ColumnContainer } from "../components/ColumnContainer";
-import { Card } from "../components/Card";
-import { GraphContainer } from "../components/GraphContainer";
-import { LineGraph } from "../components/LineGraph";
-
-// data
-import { minorsFoundData } from "../data/minorsFound";
-import { inspectionsData } from "../data/inspections";
-
-// data
+// routes
 import { routes } from "../routes/routes";
 
-// types
-import {
-  InspectionsDataType,
-  ChildrenAndTeenagersFoundDataType,
-} from "../types/types";
-
-// helpers
-import { addId } from "../helpers/addId";
-import { reduceData } from "../helpers/reduceData";
-
 export const Dashboard = () => {
-  const [inspections, setInspections] = useState<InspectionsDataType[]>([]);
-  const [minorsFound, setMinorsFound] = useState<
-    ChildrenAndTeenagersFoundDataType[]
-  >([]);
-  const [isDataInspectionDisplayed, setIsDataInspectionDisplayed] = useState<
-    boolean | undefined
-  >();
-
-  // once page is loaded, set the states
-  useEffect(() => {
-    const childAndTeenagersData = addId(minorsFoundData);
-    setMinorsFound(childAndTeenagersData);
-
-    const inspectionsDt = addId(inspectionsData);
-    setInspections(inspectionsDt);
-
-    setIsDataInspectionDisplayed(true);
-
-    // return () => {}
-  }, []);
-
-  // handle events
-  function handleButtonClick(e: SyntheticEvent<HTMLButtonElement>) {
-    const target = e.currentTarget.innerHTML;
-    target === "Inspections completed"
-      ? setIsDataInspectionDisplayed(true)
-      : setIsDataInspectionDisplayed(false);
-  }
-
   return (
     <div className="dashboard">
       <Sidebar />
